9953b7f0d9ea2c4dd29befc992df3e15f69b67d5
David Samson <david.andrew.engineer@gmail.com>
2023-10-26 01:15:56 -0400

    stubbing out process for merging subtitles back into file

--- auto_subtitle.py
+++ auto_subtitle.py
@@ -2,17 +2,74 @@ import subprocess
 import argparse
 from tqdm import tqdm
 from pathlib import Path
+import shutil
 from archytas.agent import Agent, Message
 import asyncio
 
 import pdb
 
 
-def main(directory_path):
-    for file in tqdm(Path(directory_path).glob(f"*.mkv")):
-        extract_subtitles_from_mkv(file)
-        for subtitle_file in file.parent.glob(f"{file.stem}_track*.srt"):
+language_codes = {
+    "English": "en",
+    "Chinese": "zh",
+    "Chinese (Simplified)": "zh-CN",
+    "Chinese (Traditional)": "zh-TW",
+    "French": "fr",
+    "German": "de",
+    "Italian": "it",
+    "Japanese": "ja",
+    "Korean": "ko",
+    "Portuguese": "pt",
+    "Russian": "ru",
+    "Spanish": "es",
+    "Turkish": "tr",
+}
+
+language = "Chinese (Simplified)"
+language_code = language_codes[language]
+
+def main(indir:Path, outdir:Path):
+    #create a temp directory
+    tmpfile = Path.cwd()/'temp'/"temp.mkv"
+    if not tmpfile.parent.exists():
+        tmpfile.parent.mkdir()
+    
+    for file in tqdm(sorted(indir.glob(f"*.mkv"))):
+        #copy the file to a temp directory
+        shutil.copy(file, tmpfile)
+
+        extract_subtitles_from_mkv(tmpfile)
+        for subtitle_file in tmpfile.parent.glob(f"*.srt"):
             translate_subtitles(subtitle_file)
+
+        #merge subtitles back into the mkv
+        merge_subtitles_into_mkv(tmpfile)
+
+        #move the file back to the original directory
+        pdb.set_trace()
+        # shutil.move(tmpfile, outdir/file.name)
+
+        #delete the temp directory contents
+        for f in tmpfile.parent.glob('*'):
+            f.unlink()
+
+def merge_subtitles_into_mkv(file:Path):
+    # get the new subtitle files for the language
+    new_subtitle_files = file.parent.glob(f"*.srt")
+
+    # merge the new subtitle files into the mkv
+    for i, subtitle_file in enumerate(new_subtitle_files):
+        shutil.move(file, file.parent/f"old.mkv")
+        result = subprocess.run([
+            "mkvmerge", "-o", str(file), str(file.parent/'old.mkv'), 
+            '--language', f'0:{language_code}',
+            # '--track-name', f'0:{language} {i}', 
+            str(subtitle_file)
+        ], capture_output=True)
+        if result.returncode != 0:
+            raise Exception(f"Failed to merge subtitles into {file}.\nStdout: {result.stdout}.\nStderr: {result.stderr}")
+
+
 
 def extract_subtitles_from_mkv(file:Path):
     # determine which tracks are subtitles
@@ -28,9 +85,10 @@ def extract_subtitles_from_mkv(file:Path):
             result = subprocess.run(["mkvextract", "tracks", str(file), f"{track_id}:{subtitle_file}"], capture_output=True)
             if result.returncode != 0:
                 raise Exception(f"Failed to extract subtitles from {file}.\nStdout: {result.stdout}.\nStderr: {result.stderr}")
+            
 
-def check_is_english(text:str):
-    agent35 = SynchronousAgent(model='gpt-3.5-turbo')
+def is_english(text:str):
+    agent35 = SynchronousAgent(model='gpt-3.5-turbo', spinner=None)
     if len(text) > 1000:
         text = text[:1000] + '...\n<rest of text truncated>'
     result = agent35.oneshot(prompt='You are a helpful assistant', query=f'Here is a .srt file:\n{text}\nAre these subtitles English? Please answer "yes" or "no" without any other comments.')
@@ -45,10 +103,9 @@ def check_is_english(text:str):
 
 def translate_subtitles(subtitle_file:Path):
     text = subtitle_file.read_text()
-    if check_is_english(text):
-        print(f'{subtitle_file} is in English.')
-    else:
-        print(f'{subtitle_file} is not in English.')
+    if not is_english(text):
+        print(f"Skipping {subtitle_file} because it is not in English.")
+        return
 
     #check if is english with gpt3.5
     #if it is, translate to mandarin
@@ -92,6 +149,7 @@ class SynchronousAgent:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Extract subtitles from MKV files in a specified directory.")
-    parser.add_argument("directory", help="Path to the directory containing MKV files")
+    parser.add_argument("--indir", '-i', help="Path to the directory containing .mkv files")
+    parser.add_argument("--outdir", '-o', help="Path to the directory where the processed .mkv files will be saved", default='output')
     args = parser.parse_args()
-    main(args.directory)
+    main(Path(args.indir), Path(args.outdir))
